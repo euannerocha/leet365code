bd1168368e35b149bfa275a689a6fb9eddc01928
euannerocha <annelisesr@gmail.com>
2025-01-09 00:46:52 -0300

    argumentsLength

---
+++ day9.tsx
@@ -0,0 +1,43 @@
+//SOLUTION
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length;
+}
+
+//TESTING
+
+console.log(argumentsLength(5)); 
+console.log(argumentsLength({}, null, "3")); 
+console.log(argumentsLength()); 
+
+// MY EXPLANATION ABOUT THE CODE
+  
+// 😒
+  
+// EXERCISE
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+// Example 1:
+
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+
+// One value was passed to the function so it should return 1.
+// Example 2:
+
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+
+// Constraints:
+
+// args is a valid JSON array
+// 0 <= args.length <= 100
