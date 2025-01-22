db82df6ac98087f5ede38e321c9e047559019cfe
euannerocha <annelisesr@gmail.com>
2025-01-22 16:07:34 -0300

    day22: chunck

--- January/day22.tsx
+++ January/day22.tsx
@@ -1,9 +1,62 @@
 //SOLUTION
 
+type JSONValues = null | boolean | number | string | JSONValues[] | { [key: string]: JSONValues };
+
+function chunk(arr: JSONValues[], size: number): JSONValues[][] {
+    const result: JSONValues[][] = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+
+    return result;
+}
+
+
 //TESTING  
+
+console.log(chunk([1, 2, 3, 4, 5], 1)); 
+console.log(chunk([1, 9, 6, 3, 2], 3));
+console.log(chunk([8, 5, 3, 2, 6], 6));
+console.log(chunk([], 1)); 
 
 // MY EXPLANATION ABOUT THE CODE
 
 // 😒
 
 // EXERCISE
+
+// Given an array arr and a chunk size size, return a chunked array.
+
+// A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+
+// You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
+
+// Please solve it without using lodash's _.chunk function.
+
+// Example 1:
+
+// Input: arr = [1,2,3,4,5], size = 1
+// Output: [[1],[2],[3],[4],[5]]
+// Explanation: The arr has been split into subarrays each with 1 element.
+// Example 2:
+
+// Input: arr = [1,9,6,3,2], size = 3
+// Output: [[1,9,6],[3,2]]
+// Explanation: The arr has been split into subarrays with 3 elements. However, only two elements are left for the 2nd subarray.
+// Example 3:
+
+// Input: arr = [8,5,3,2,6], size = 6
+// Output: [[8,5,3,2,6]]
+// Explanation: Size is greater than arr.length thus all elements are in the first subarray.
+// Example 4:
+
+// Input: arr = [], size = 1
+// Output: []
+// Explanation: There are no elements to be chunked so an empty array is returned.
+ 
+// Constraints:
+
+// arr is a valid JSON array
+// 2 <= JSON.stringify(arr).length <= 105
+// 1 <= size <= arr.length + 1
