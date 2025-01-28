3c43e2e6831fc388f5f4c486101b16ec08b00a20
euannerocha <annelisesr@gmail.com>
2025-01-28 18:06:23 -0300

    day28: eventEmitter

--- January/day28.tsx
+++ January/day28.tsx
@@ -1,6 +1,49 @@
 //SOLUTION
 
+type Callback = (...args: any[]) => any;
+type Subscription = {
+    unsubscribe: () => void;
+};
+
+class EventEmitter {
+    private events: Map<string, Callback[]>;
+
+    constructor() {
+        this.events = new Map();
+    }
+
+    subscribe(eventName: string, callback: Callback): Subscription {
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+
+        const callbacks = this.events.get(eventName)!;
+        callbacks.push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1) {
+                    callbacks.splice(index, 1);
+                }
+            },
+        };
+    }
+
+    emit(eventName: string, args: any[] = []): any[] {
+        const callbacks = this.events.get(eventName) || [];
+        return callbacks.map(callback => callback(...args));
+    }
+}
+
 //TESTING 
+
+const emitterInstance = new EventEmitter();
+function onClickCallback() { return 99; }
+const sub = emitterInstance.subscribe('onClick', onClickCallback);
+emitterInstance.emit('onClick');
+sub.unsubscribe();
+emitterInstance.emit('onClick');
 
 // MY EXPLANATION ABOUT THE CODE
 
@@ -19,43 +62,54 @@
 
 // Example 1:
 
-// Input: 
+// Input:
+ 
 // actions = ["EventEmitter", "emit", "subscribe", "subscribe", "emit"], 
 // values = [[], ["firstEvent"], ["firstEvent", "function cb1() { return 5; }"],  ["firstEvent", "function cb1() { return 6; }"], ["firstEvent"]]
 // Output: [[],["emitted",[]],["subscribed"],["subscribed"],["emitted",[5,6]]]
+
 // Explanation: 
+
 // const emitter = new EventEmitter();
 // emitter.emit("firstEvent"); // [], no callback are subscribed yet
 // emitter.subscribe("firstEvent", function cb1() { return 5; });
 // emitter.subscribe("firstEvent", function cb2() { return 6; });
 // emitter.emit("firstEvent"); // [5, 6], returns the output of cb1 and cb2
+
 // Example 2:
 
 // Input: 
+
 // actions = ["EventEmitter", "subscribe", "emit", "emit"], 
 // values = [[], ["firstEvent", "function cb1(...args) { return args.join(','); }"], ["firstEvent", [1,2,3]], ["firstEvent", [3,4,6]]]
 // Output: [[],["subscribed"],["emitted",["1,2,3"]],["emitted",["3,4,6"]]]
+
 // Explanation: Note that the emit method should be able to accept an OPTIONAL array of arguments.
 
 // const emitter = new EventEmitter();
 // emitter.subscribe("firstEvent, function cb1(...args) { return args.join(','); });
 // emitter.emit("firstEvent", [1, 2, 3]); // ["1,2,3"]
 // emitter.emit("firstEvent", [3, 4, 6]); // ["3,4,6"]
+
 // Example 3:
 
 // Input: 
+
 // actions = ["EventEmitter", "subscribe", "emit", "unsubscribe", "emit"], 
 // values = [[], ["firstEvent", "(...args) => args.join(',')"], ["firstEvent", [1,2,3]], [0], ["firstEvent", [4,5,6]]]
 // Output: [[],["subscribed"],["emitted",["1,2,3"]],["unsubscribed",0],["emitted",[]]]
+
 // Explanation:
 // const emitter = new EventEmitter();
 // const sub = emitter.subscribe("firstEvent", (...args) => args.join(','));
 // emitter.emit("firstEvent", [1, 2, 3]); // ["1,2,3"]
 // sub.unsubscribe(); // undefined
 // emitter.emit("firstEvent", [4, 5, 6]); // [], there are no subscriptions
+
 // Example 4:
 
 // Input: 
+
 // actions = ["EventEmitter", "subscribe", "subscribe", "unsubscribe", "emit"], 
 // values = [[], ["firstEvent", "x => x + 1"], ["firstEvent", "x => x + 2"], [0], ["firstEvent", [5]]]
 // Output: [[],["subscribed"],["subscribed"],["unsubscribed",0],["emitted",[7]]]
