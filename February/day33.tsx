5e67e79348ac68c971b24785d912880ae849c0eb
Anne <106755882+euannerocha@users.noreply.github.com>
2025-02-03 18:05:44 -0300

    Update day33.tsx

--- February/day33.tsx
+++ February/day33.tsx
@@ -1,7 +1,36 @@
 //LIST SOLUTIONS
 
-// 5 more exercises 
+// Write a program that recieve an Integer and return the doble of this number 
 
+function double(n: number): number{
+    if (!Number.isInteger(n)){
+        throw new Error("Use only integers numbers");
+    };
+
+    const result = n*2;
+
+    return result;
+}
+
+double(2);
+double(4);
+double(2345);
+
+// Write a program that convert Celsius to Fahrenheit
+
+function celciusToFahrenheit(c: number): number{
+    if(isNaN(c)){
+        throw new Error("Use just numbers");
+    };
+
+    const fahrenheit =  (c * 9/5) + 32;
+
+    return fahrenheit;
+}
+
+celciusToFahrenheit(3);
+celciusToFahrenheit(0);
+celciusToFahrenheit(3.2);
 
 // MY EXPLANATION ABOUT THE CODE
 
