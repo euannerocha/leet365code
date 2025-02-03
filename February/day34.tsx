748fd7dda5cbebb3733349765b32ab500fa7cb1b
Anne <106755882+euannerocha@users.noreply.github.com>
2025-02-03 18:10:49 -0300

    Create day34.tsx

---
+++ February/day34.tsx
@@ -0,0 +1,16 @@
+//SOLUTION
+
+//Write a program that calculates a triangle area
+
+function calculateTriangleArea(b: number, h: number): number{
+    if (isNaN(b) || isNaN(h)){
+        throw new Error("Use just numbers");
+    };
+
+    const area = b * h /2;
+
+    return area;
+}
+
+calculateTriangleArea(3, 2);
+calculateTriangleArea(7, 9);
