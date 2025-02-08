84fd72ca3bac471a86d22c9fc20cd2f8049232ce
euannerocha <annelisesr@gmail.com>
2025-02-07 21:09:04 -0300

    day38: discount

---
+++ February/day38.tsx
@@ -0,0 +1,12 @@
+//SOLUTION
+
+//Write a program that receive a number and verify if it is zero, positive or negative
+
+function discount(number: number): number {
+    const calcDiscount = number * 0.90
+    return calcDiscount;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
