aaeef2146a1988bdcb8dc83d55b161d60c49b62c
euannerocha <annelisesr@gmail.com>
2025-02-08 18:26:07 -0300

    day39: factorial

---
+++ February/day39.tsx
@@ -0,0 +1,15 @@
+//SOLUTION
+
+//Write a program that receive a number and verify if it is zero, positive or negative
+
+function factorial(number: number): number {
+    let count = 1;
+    for (let i = number; i > 0; i--) {
+        count *= i;
+    }
+    return count;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
