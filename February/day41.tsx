adad475645efdba00a4c8be3614f54bc83bb88eb
euannerocha <annelisesr@gmail.com>
2025-02-10 22:19:54 -0300

    day40: imc

---
+++ February/day41.tsx
@@ -0,0 +1,12 @@
+//SOLUTION
+
+//Write a program that recieve weight and hight and calculates the IMC
+
+function imc(w: number, h: number): number {
+    let calcImc = w/(h*h);
+    return calcImc;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
