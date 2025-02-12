375681f380f17ec4bf86a1afab87f1fc828cd9e9
Anne <106755882+euannerocha@users.noreply.github.com>
2025-02-12 20:50:08 -0300

    day43: speed

---
+++ February/day43.tsx
@@ -0,0 +1,13 @@
+//SOLUTION
+
+//Write a program that recieve the speed and the speed limit and returns if exceed the speed limit or not
+
+function speed(s: number, l: number): boolean {
+    let isAbove = s>l;
+
+    return isAbove;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
