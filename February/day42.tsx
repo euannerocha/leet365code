7f8d0d0dea4d6959142ef2edd4d251156abd86f7
euannerocha <annelisesr@gmail.com>
2025-02-11 22:36:36 -0300

    day42: welcome

---
+++ February/day42.tsx
@@ -0,0 +1,12 @@
+//SOLUTION
+
+//Write a program that recieve a name and returns a personalized welcome
+
+function welcome(name: string): string {
+    let welcomePersonalized = `Welcome, ${name}!`
+    return welcomePersonalized;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
