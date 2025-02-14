82e3ecd2abc82bc271b9f7220fa41938b398d14e
euannerocha <annelisesr@gmail.com>
2025-02-13 22:08:04 -0300

    day44: salary

---
+++ February/day44.tsx
@@ -0,0 +1,14 @@
+//SOLUTION
+
+//Write a program that recieve a salary and increase 15%
+
+function salary(s: number): number {
+    let calcIncrease = s * 0.15;
+    let calcSalary = s += calcIncrease;
+
+    return calcSalary;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
