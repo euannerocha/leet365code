9a3e9f3fdc1b0eed35f6f379af7ed11d2c7218d0
euannerocha <annelisesr@gmail.com>
2025-02-14 21:02:05 -0300

    largest

---
+++ February/day45.tsx
@@ -0,0 +1,14 @@
+//SOLUTION
+
+//Write a program that recieves 3 numbers and return the largest one
+
+function largest(n: number, k: number, l: number): number {
+    let arrNumbers = [n, k, l];
+    let theLargest = Math.max(...arrNumbers);
+
+    return theLargest;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
