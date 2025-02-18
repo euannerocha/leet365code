71277326dcb81fe6c0fbcf1b96d597e491a0fc84
euannerocha <annelisesr@gmail.com>
2025-02-17 21:30:15 -0300

    day48: biggestNumber

---
+++ February/day48.tsx
@@ -0,0 +1,15 @@
+//SOLUTION
+
+//Write a program that recieves 2 numbers and return the biggest
+
+function biggest(n: number, k: number): number {
+
+    let biggestNumber = Math.max(n,k)
+
+    return biggestNumber;
+    
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
