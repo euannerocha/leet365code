8977d3fedbd9313a1000ad3cff7c15143e9e8faa
euannerocha <annelisesr@gmail.com>
2025-02-19 23:04:59 -0300

    day50: fibbonacci

---
+++ February/day50.tsx
@@ -0,0 +1,20 @@
+//SOLUTION
+
+//Write a program that recieves a number and return the Fibonnacci sequence till this number
+
+function fibbonacciNumber(n: number): number[] {
+    const sequence: number[] = [];
+
+    let a = 0, b = 1;
+
+    while (a <= n) {
+        sequence.push(a);
+        [a, b] = [b, a + b];
+    }
+
+    return sequence;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
