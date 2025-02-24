8510784343d948af0d823b2afd687ff99c988f7c
euannerocha <annelisesr@gmail.com>
2025-02-23 22:52:00 -0300

    day54: random numbers

--- February/day54.tsx
+++ February/day54.tsx
@@ -1,1 +1,16 @@
-//meu aniversário 
+//SOLUTION
+
+//Write a program that simulate 2 random numbers and sum them
+
+function sumNumbers(): number {
+    let n = Math.random(); 
+    let k = Math.random(); 
+
+    let sumNumbers = n + k;
+
+    return sumNumbers;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
