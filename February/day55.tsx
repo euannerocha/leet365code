a580736094156bfdd6eaebfad06db0d2c31f3cc7
euannerocha <annelisesr@gmail.com>
2025-02-24 21:23:30 -0300

    day55: reverse

---
+++ February/day55.tsx
@@ -0,0 +1,13 @@
+//SOLUTION
+
+//Write a program that receives an array and returns the reverse
+
+function reverse(n: any[]): any[] {
+    let reverse = n.reverse();
+
+    return reverse;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
