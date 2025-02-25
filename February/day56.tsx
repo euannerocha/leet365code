712b99893066bdc0325713bedd94eaebe96cc967
euannerocha <annelisesr@gmail.com>
2025-02-25 20:03:29 -0300

    day56: biggest

---
+++ February/day56.tsx
@@ -0,0 +1,13 @@
+//SOLUTION
+
+//Write a program that receives an array with weights and returns the biggest
+
+function weights(n: number[]): number {
+    let biggest = Math.max(...n);
+
+    return biggest;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
