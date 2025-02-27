5b6842f931c723cc48b9a11f5eac7c75006c30cd
euannerocha <annelisesr@gmail.com>
2025-02-26 22:40:55 -0300

    day57 sort

---
+++ February/day57.tsx
@@ -0,0 +1,13 @@
+//SOLUTION
+
+//Write a program that receives an array with letters and returns the alphabetic order
+
+function letters(l: string[]): string[] {
+    let alphabeticOrder = l.sort();
+
+    return alphabeticOrder;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
