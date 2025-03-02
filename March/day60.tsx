dca0fb89278ae87142480c3f6b8c92c5d293ede1
euannerocha <annelisesr@gmail.com>
2025-03-01 23:38:34 -0300

    day60: sum products

---
+++ March/day60.tsx
@@ -0,0 +1,15 @@
+//SOLUTION
+
+//Write a program that recieves a list of products and return the sum
+function sumProducts(p: number[]): number {
+    let count = 0;
+    for (let i = 0; i = p.length; i++) {
+        return count ++;
+    };
+
+    return count;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
