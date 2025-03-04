80c1410bcde26cab6e224ce92dfa754506dbf1cd
euannerocha <annelisesr@gmail.com>
2025-03-03 22:02:51 -0300

    day62: count letters

---
+++ March/day62.tsx
@@ -0,0 +1,13 @@
+//SOLUTION
+
+//Write a program that recieves a word and return the number of characters
+
+function lengthString(s: string): number {
+    let countLetters = s.length
+
+    return countLetters;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
