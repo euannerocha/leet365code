ec44655dbd9a83ebeea22f4f33c47e671a92afa7
euannerocha <annelisesr@gmail.com>
2025-03-05 22:06:24 -0300

    day64: min number

---
+++ March/day64.tsx
@@ -0,0 +1,13 @@
+//SOLUTION
+
+//Write a program that recieves values and return the minimum
+
+function lowestNumber(n: number[]): number {
+    let lowest = Math.min(...n);
+
+    return lowest;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
