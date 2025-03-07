8ce3875064cb036d735954aabce61f86ed01ec4a
euannerocha <annelisesr@gmail.com>
2025-03-06 22:57:58 -0300

    day65: taxes

---
+++ March/day65.tsx
@@ -0,0 +1,12 @@
+//SOLUTION
+
+//Write a program that recieves a value and return the value with 5% more after 12 months
+
+function taxes(n: number): number {
+    let calc = n * 1.05 ** 12;
+    return calc;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
