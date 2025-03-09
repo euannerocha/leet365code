c794ee0c58416434f75193f0d42cf795bb1589c6
euannerocha <annelisesr@gmail.com>
2025-03-08 23:52:19 -0300

    day67.tsx

---
+++ March/day67.tsx
@@ -0,0 +1,18 @@
+//SOLUTION
+
+//Write a program that recieves values and return the sum
+
+function reader(p: number[]): number {
+
+    let newArr: number = 0;
+
+    p.map((n) => {
+        newArr = n++
+    });
+
+    return newArr;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
