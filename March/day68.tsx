700cb17e07c03d67334c4ecef489ca75187955a4
euannerocha <annelisesr@gmail.com>
2025-03-09 00:21:15 -0300

    day68: pi

---
+++ March/day68.tsx
@@ -0,0 +1,14 @@
+//SOLUTION
+
+//Write a program that returns the number PI 
+
+function pi(): number {
+
+    let pi = Math.PI
+
+    return pi;
+}
+
+// MY EXPLANATION ABOUT THE CODE
+
+//I'm doing a basic exercises list, to practice.
