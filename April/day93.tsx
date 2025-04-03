967e7e45cb59ad109abeb0bae637b87ac4b281f7
euannerocha <annelisesr@gmail.com>
2025-04-03 00:11:28 -0300

    day93: close strings

--- April/day93.tsx
+++ April/day93.tsx
@@ -1,5 +1,27 @@
 //SOLUTION
 
+function closeStrings(word1: string, word2: string): boolean {
+    if (word1.length !== word2.length) return false;
+    
+    const freq1 = new Map<string, number>();
+    const freq2 = new Map<string, number>();
+    
+    for (const char of word1) {
+        freq1.set(char, (freq1.get(char) || 0) + 1);
+    }
+    for (const char of word2) {
+        freq2.set(char, (freq2.get(char) || 0) + 1);
+    }
+    
+    const keys1 = [...freq1.keys()].sort();
+    const keys2 = [...freq2.keys()].sort();
+    if (keys1.join('') !== keys2.join('')) return false;
+    
+    const values1 = [...freq1.values()].sort((a, b) => a - b);
+    const values2 = [...freq2.values()].sort((a, b) => a - b);
+    
+    return values1.join('') === values2.join('');
+}
 
 // MY EXPLANATION ABOUT THE CODE
 //I'm doing a list of LeetCode exercises (75 in total)
