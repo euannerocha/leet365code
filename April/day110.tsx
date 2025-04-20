17d6607486185d865ef55275925b3b6d8b295fdf
Anne <106755882+euannerocha@users.noreply.github.com>
2025-04-20 10:04:20 -0300

    Criar day110.tsx

---
+++ April/day110.tsx
@@ -0,0 +1,25 @@
+type Produto = {
+  nome: string;
+  preco: number;
+  disponivel: boolean;
+};
+
+const produtos: Produto[] = [
+  { nome: "café", preco: 9.5, disponivel: true },
+  { nome: "chá", preco: 5.75, disponivel: false },
+  { nome: "bolo", preco: 12, disponivel: true }
+];
+
+
+const catalogoFormatado = produtos.map((produto) => {
+  return {
+    nome: produto.nome.toUpperCase(),
+    preco: produto.preco.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    }),
+    status: produto.disponivel ? "Disponível" : "Indisponível"
+  };
+});
+
+console.log(catalogoFormatado);
