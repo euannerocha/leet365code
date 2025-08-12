060cd396d911a322a03644eed59c5867b3b1a6fa
Anne <106755882+euannerocha@users.noreply.github.com>
2025-08-12 12:39:12 -0300

    Criar day224.tsx

---
+++ day224.tsx
@@ -0,0 +1,29 @@
+// Solution
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_map:
+                return [num_map[diff], i]
+            num_map[num] = i
+
+
+//Exercise
+
+//Title: Two Sum
+//Difficulty: Easy
+
+//Description:
+Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Input: nums = [2,7,11,15], target = 9  
+Output: [0,1]
+
+Explanation: nums[0] + nums[1] = 2 + 7 = 9.//
+
