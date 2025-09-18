193d978a333b390ad4741a8cae5d90e5c04ab65b
Anne <106755882+euannerocha@users.noreply.github.com>
2025-09-18 00:00:52 -0300

    Atualizar o day261.py

--- day261.py
+++ day261.py
@@ -1,25 +1,18 @@
-def find_median_sorted_arrays(nums1, nums2):
-    if len(nums1) > len(nums2):
-        nums1, nums2 = nums2, nums1
-    m, n = len(nums1), len(nums2)
-    imin, imax, half = 0, m, (m + n + 1) // 2
-    while imin <= imax:
-        i = (imin + imax) // 2
-        j = half - i
-        if i < m and nums2[j-1] > nums1[i]:
-            imin = i + 1
-        elif i > 0 and nums1[i-1] > nums2[j]:
-            imax = i - 1
+def trap(height):
+    if not height:
+        return 0
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+    water = 0
+    while left < right:
+        if left_max < right_max:
+            left += 1
+            left_max = max(left_max, height[left])
+            water += left_max - height[left]
         else:
-            if i == 0: max_left = nums2[j-1]
-            elif j == 0: max_left = nums1[i-1]
-            else: max_left = max(nums1[i-1], nums2[j-1])
-            if (m + n) % 2 == 1:
-                return max_left
-            if i == m: min_right = nums2[j]
-            elif j == n: min_right = nums1[i]
-            else: min_right = min(nums1[i], nums2[j])
-            return (max_left + min_right) / 2
+            right -= 1
+            right_max = max(right_max, height[right])
+            water += right_max - height[right]
+    return water
 
-print(find_median_sorted_arrays([1,3], [2]))
-print(find_median_sorted_arrays([1,2], [3,4]))
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
