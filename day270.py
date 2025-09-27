a3cbbac027803b77355cedc0764c50be45078e7e
Anne <106755882+euannerocha@users.noreply.github.com>
2025-09-27 17:37:04 -0300

    Criar day270.py

---
+++ day270.py
@@ -0,0 +1,17 @@
+def minSubArrayLen(target, nums):
+    n = len(nums)
+    left = 0
+    total = 0
+    min_len = float('inf')
+    
+    for right in range(n):
+        total += nums[right]
+        while total >= target:
+            min_len = min(min_len, right - left + 1)
+            total -= nums[left]
+            left += 1
+    
+    return 0 if min_len == float('inf') else min_len
+
+print(minSubArrayLen(7, [2,3,1,2,4,3])) 
+print(minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
