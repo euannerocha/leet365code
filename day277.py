ec082646360e03e116e1d689b88a4f5348ec2fe3
Anne <106755882+euannerocha@users.noreply.github.com>
2025-10-04 19:32:20 -0300

    Criar day277.py

---
+++ day277.py
@@ -0,0 +1,18 @@
+function rotate(matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+  return matrix;
+}
+
+console.log(rotate([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]));
