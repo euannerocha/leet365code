2703940310f28a4dcefe39e410ac8d53ad48c902
Anne <106755882+euannerocha@users.noreply.github.com>
2025-10-06 21:10:02 -0300

    Criar day279.js

---
+++ day279.js
@@ -0,0 +1,17 @@
+function productExceptSelf(nums) {
+  const n = nums.length;
+  const res = Array(n).fill(1);
+  let prefix = 1;
+  for (let i = 0; i < n; i++) {
+    res[i] = prefix;
+    prefix *= nums[i];
+  }
+  let suffix = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    res[i] *= suffix;
+    suffix *= nums[i];
+  }
+  return res;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4]));
