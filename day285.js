2821ba2a798e641cb0707181d8cb7f1c6bbc9caa
Anne <106755882+euannerocha@users.noreply.github.com>
2025-10-13 14:54:13 -0300

    Criar day285.js

---
+++ day285.js
@@ -0,0 +1,15 @@
+function mergeIntervals(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res = [intervals[0]];
+  for (let i = 1; i < intervals.length; i++) {
+    const last = res[res.length - 1];
+    if (intervals[i][0] <= last[1]) {
+      last[1] = Math.max(last[1], intervals[i][1]);
+    } else {
+      res.push(intervals[i]);
+    }
+  }
+  return res;
+}
+
+console.log(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]));
