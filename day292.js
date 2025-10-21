b6bd7c1e6a3ac374b4c72e0fecee6a4a2f869b1f
Anne <106755882+euannerocha@users.noreply.github.com>
2025-10-21 18:04:59 -0300

    Criar day292.js

---
+++ day292.js
@@ -0,0 +1,14 @@
+function maxProduct(nums) {
+  let maxProd = nums[0];
+  let currMax = nums[0];
+  let currMin = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < 0) [currMax, currMin] = [currMin, currMax];
+    currMax = Math.max(nums[i], currMax * nums[i]);
+    currMin = Math.min(nums[i], currMin * nums[i]);
+    maxProd = Math.max(maxProd, currMax);
+  }
+  return maxProd;
+}
+
+console.log(maxProduct([2,3,-2,4]));
