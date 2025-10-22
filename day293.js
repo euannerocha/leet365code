8e4c01d94ebcf748704f519dcd0125478245574e
Anne <106755882+euannerocha@users.noreply.github.com>
2025-10-22 17:05:50 -0300

    Criar day293.js

---
+++ day293.js
@@ -0,0 +1,10 @@
+function canJump(nums) {
+  let reach = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > reach) return false;
+    reach = Math.max(reach, i + nums[i]);
+  }
+  return true;
+}
+
+console.log(canJump([2,3,1,1,4]));
