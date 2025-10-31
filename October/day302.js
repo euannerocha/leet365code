91e146a54abd0e4373ab2bf5a22dae2ff26adccb
Anne <106755882+euannerocha@users.noreply.github.com>
2025-10-31 17:53:18 -0300

    Criar day302.js

---
+++ October/day302.js
@@ -0,0 +1,17 @@
+function rotateArray(nums, k) {
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+  return nums;
+}
+
+function reverse(arr, start, end) {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+}
+
+console.log(rotateArray([1,2,3,4,5,6,7], 3));
