adf90aa3ecc111a9f7020d831903e66e05517b85
Anne <106755882+euannerocha@users.noreply.github.com>
2025-11-05 21:26:16 -0300

    Criar day307.js

---
+++ day307.js
@@ -0,0 +1,21 @@
+function insertInterval(intervals, newInterval) {
+  const res = [];
+  let i = 0;
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    res.push(intervals[i]);
+    i++;
+  }
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  res.push(newInterval);
+  while (i < intervals.length) {
+    res.push(intervals[i]);
+    i++;
+  }
+  return res;
+}
+
+console.log(insertInterval([[1,3],[6,9]], [2,5]));
