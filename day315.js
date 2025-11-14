03b924fd040f5088a7e43ff1ce759d7545204ce1
Anne <106755882+euannerocha@users.noreply.github.com>
2025-11-13 21:22:53 -0300

    Criar day315.js

---
+++ day315.js
@@ -0,0 +1,13 @@
+function firstNonRepeatingChar(s) {
+  const freq = {};
+
+  for (const char of s) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  for (const char of s) {
+    if (freq[char] === 1) return char;
+  }
+
+  return "_";
+}
