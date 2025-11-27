79c9ebb33f2d4c3e933be74258e872e1338fa73f
Anne <106755882+euannerocha@users.noreply.github.com>
2025-11-26 21:09:29 -0300

    Criar day327.js

---
+++ day327.js
@@ -0,0 +1,15 @@
+function maxArea(height) {
+  let l = 0;
+  let r = height.length - 1;
+  let max = 0;
+
+  while (l < r) {
+    const h = Math.min(height[l], height[r]);
+    const area = h * (r - l);
+    if (area > max) max = area;
+    if (height[l] < height[r]) l++;
+    else r--;
+  }
+
+  return max;
+}
