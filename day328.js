fbef092c67cbb079c751e5c492fee7f312348b6b
Anne <106755882+euannerocha@users.noreply.github.com>
2025-11-27 21:32:30 -0300

    Criar day328.js

---
+++ day328.js
@@ -0,0 +1,14 @@
+function dailyTemperatures(temperatures) {
+  const res = Array(temperatures.length).fill(0);
+  const stack = [];
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+      const idx = stack.pop();
+      res[idx] = i - idx;
+    }
+    stack.push(i);
+  }
+
+  return res;
+}
