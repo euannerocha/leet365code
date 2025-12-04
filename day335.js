4a8256b02e88704720253eb429d2b23f20dd7191
Anne <106755882+euannerocha@users.noreply.github.com>
2025-12-04 17:39:27 -0300

    Criar day335.js

---
+++ day335.js
@@ -0,0 +1,13 @@
+function subarraySum(nums, k) {
+  const map = { 0: 1 };
+  let sum = 0;
+  let count = 0;
+
+  for (let n of nums) {
+    sum += n;
+    if (map[sum - k]) count += map[sum - k];
+    map[sum] = (map[sum] || 0) + 1;
+  }
+
+  return count;
+}
