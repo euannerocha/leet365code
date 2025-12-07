f73787fb9cb2c0d23942db747c2ce5af24b2fdb3
Anne <106755882+euannerocha@users.noreply.github.com>
2025-12-07 20:04:32 -0300

    Criar day338.js

---
+++ day338.js
@@ -0,0 +1,16 @@
+function intervalIntersection(A, B) {
+  let i = 0, j = 0;
+  const res = [];
+
+  while (i < A.length && j < B.length) {
+    const start = Math.max(A[i][0], B[j][0]);
+    const end = Math.min(A[i][1], B[j][1]);
+
+    if (start <= end) res.push([start, end]);
+
+    if (A[i][1] < B[j][1]) i++;
+    else j++;
+  }
+
+  return res;
+}
