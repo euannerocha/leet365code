8b671426d48af35babccd2ed78c1f61866beb2fe
Anne <106755882+euannerocha@users.noreply.github.com>
2025-12-10 21:02:10 -0300

    Criar day341.js

---
+++ day341.js
@@ -0,0 +1,22 @@
+function threeSumClosest(nums, target) {
+  nums.sort((a, b) => a - b);
+  let closest = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+
+      if (Math.abs(sum - target) < Math.abs(closest - target)) {
+        closest = sum;
+      }
+
+      if (sum > target) r--;
+      else l++;
+    }
+  }
+
+  return closest;
+}
