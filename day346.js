4dba1a42212c2683f70267442978b14d5c4157f3
Anne <106755882+euannerocha@users.noreply.github.com>
2025-12-17 00:08:06 -0300

    Criar day346.js

---
+++ day346.js
@@ -0,0 +1,18 @@
+function maxSlidingWindow(nums, k) {
+  const deque = [];
+  const res = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (deque.length && deque[0] <= i - k) deque.shift();
+
+    while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+
+    deque.push(i);
+
+    if (i >= k - 1) res.push(nums[deque[0]]);
+  }
+
+  return res;
+}
