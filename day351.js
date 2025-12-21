692d08917a9431ce62328bd04a3406536d15e738
Anne <106755882+euannerocha@users.noreply.github.com>
2025-12-21 20:38:24 -0300

    Criar day351.js

---
+++ day351.js
@@ -0,0 +1,19 @@
+function findMaxLength(nums) {
+  const map = new Map();
+  map.set(0, -1);
+
+  let count = 0;
+  let maxLen = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] === 1 ? 1 : -1;
+
+    if (map.has(count)) {
+      maxLen = Math.max(maxLen, i - map.get(count));
+    } else {
+      map.set(count, i);
+    }
+  }
+
+  return maxLen;
+}
