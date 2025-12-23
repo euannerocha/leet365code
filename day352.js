8c346fe9d51614548b31569a02512b15dea37e77
Anne <106755882+euannerocha@users.noreply.github.com>
2025-12-22 21:54:14 -0300

    Criar day352.js

---
+++ day352.js
@@ -0,0 +1,11 @@
+function maxChunksToSorted(arr) {
+  let max = 0;
+  let chunks = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    max = Math.max(max, arr[i]);
+    if (max === i) chunks++;
+  }
+
+  return chunks;
+}
